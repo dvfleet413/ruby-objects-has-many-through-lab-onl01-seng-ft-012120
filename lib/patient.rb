95084bb7b58efa50c131940a485a58bc76d85c56
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 16:47:19 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self
+  end
+  
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  
+  def self.all
+    @@all
+  end
+  
+end
