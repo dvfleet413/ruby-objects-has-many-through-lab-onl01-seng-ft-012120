c8d78d6844088ac1b505ba70181026b4e2ce73bd
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 16:40:14 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def new_appointment(patient, date)
+    
+  
+  
+  def self.all
+    @@all
+  end
+  
+  
+end
