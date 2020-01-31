4745836558bb6a1c5498df95a7b2f0e1e82b397f
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 16:17:30 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Arrtist 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.artist = self
+  end
+  
+  def genres
+    songs = Song.all.select {|song| song.artist == self}
+    songs.collect{|song| song.genre}
+  end
+  
+  def self.all 
+    @@all 
+  end
+end
