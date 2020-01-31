ce9200c84591af254bc6fd5f2a946428e975522d
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 16:25:52 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -6,6 +6,14 @@ class Genre
     @@all << self 
   end
   
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def artists
+    self.songs.collect{|song| song.artist}
+  end
+  
   def self.all
     @@all
   end
